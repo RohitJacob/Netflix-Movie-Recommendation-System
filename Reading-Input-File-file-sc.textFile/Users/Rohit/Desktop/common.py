9af1fb3819d10614d200f3466dd77b91773416e5
Rohit Jacob <RohitJacob@users.noreply.github.com>
2016-05-28 16:52:44 -0700

    Delete common.py

--- Reading-Input-File-file-sc.textFile/Users/Rohit/Desktop/common.py
+++
@@ -1,6 +0,0 @@
-#Finding Length of userlist and Common users
-def common(m1,lister,m2,lister2):
-	if(m1<m2) and m1!="" and m2!="":
-		return ((m1,m2),len(set(lister).intersection(set(lister2))) , len(set(lister)), len(set(lister2)))
-	else:
-		return 0
